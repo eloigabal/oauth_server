6e58a5ae1989679f4f4b3f2a4c9c1f415a853211
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-04-25 13:15:51 +0200

    added enforced password security

--- user_management/forms.py
+++ user_management/forms.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import password_validation
 from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+
+from flexcoop_auth_server import settings
 
 
 class SignUpForm(forms.ModelForm):
@@ -9,8 +13,20 @@ class SignUpForm(forms.ModelForm):
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password', 'password_confirm']
 
-    def clean(self):
+    def clean_password_confirm(self):
+        password1 = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password_confirm")
+        if password1 != password2:
+            self.add_error('password_confirm', "Password does not match")
+        return password2
+
+    def _post_clean(self):
+        super(SignUpForm, self)._post_clean()
+        # Validate the password after self.instance is updated with form data
+        # by super().
         password = self.cleaned_data.get('password')
-        password_confirm = self.cleaned_data.get('password_confirm')
-        if password != password_confirm:
-            raise ValidationError("Password and Password Confirm must match")
+        try:
+            password_validation.validate_password(password, self.instance)
+        except forms.ValidationError as error:
+            self.add_error('password_confirm', error)
+
